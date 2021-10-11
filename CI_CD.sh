45def66f4d2ad8f72657a8346d148b8e714b9c5e
mzk <uta.mizuki@gmail.com>
2021-10-11 20:31:51 +0900

    page generated at

--- CI_CD.sh
+++ CI_CD.sh
@@ -1,5 +1,6 @@
 git pull origin main
 sh GeneratePages.sh
-git add .
-git commit -m "page generated at ${date}"
+git add ./docs/pages
+now = date
+git commit -m "page generated at ${now}"
 git push origin main
