3d4d4d3d2378c012ac4c613f7892f947b7098edb
mzk <uta.mizuki@gmail.com>
2021-10-12 07:29:17 +0900

    change CI_CD.sh

--- CI_CD.sh
+++ CI_CD.sh
@@ -1,5 +1,10 @@
+echo "Get latest apps" 
 git pull origin main
+
+echo "Do generate"
 sh GeneratePages.sh
+
+echo "Push generated pages"
 git add ./docs/pages
 git commit -m "page generated at $(date)"
 git push origin main
