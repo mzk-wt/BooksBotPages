1d7eb4c1829bfd741ca9e963e6df24b7e5e43a2c
mzk <uta.mizuki@gmail.com>
2023-02-04 16:03:39 +0900

    再稼働

--- CI_CD.sh
+++ CI_CD.sh
@@ -3,10 +3,10 @@ cd `dirname $0`
 echo "Get latest apps" 
 git pull origin main
 
-#echo "Do generate"
-#sh GeneratePages.sh
+echo "Do generate"
+sh GeneratePages.sh
 
-#echo "Push generated pages"
-#git add .
-#git commit -m "page generated at $(date)"
-#git push origin main
+echo "Push generated pages"
+git add .
+git commit -m "page generated at $(date)"
+git push origin main
